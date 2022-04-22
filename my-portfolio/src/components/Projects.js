5efaa00f685188adfb0c15078315d6e1021b7877
Haleem Hamid <hhamidbiz@gmail.com>
2022-04-22 11:04:24 -0700

    added media to project cards

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { projects } from "./database";
-import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
--- my-portfolio/src/components/database.js
+++ my-portfolio/src/components/database.js
@@ -1,32 +1,37 @@
+import FANDOM from "../assets/FANDOM.gif"
+import MovieRater from "../assets/MovieRater.gif"
+import UrbanWalls from "../assets/UrbanWalls.gif"
+import Brewery from "../assets/Brewery.gif"
+import PluggedN from "../assets/PluggedN.gif"
 export const projects = [
   {
     name: "Plugged-N",
     stack: "React / Rails",
-    media: "",
+    media: PluggedN,
     Github: "https://github.com/HaleemH/Plugged-N",
   },
   {
     name: "The Brewery Discovery",
     stack: "React / Rails",
-    media: "",
+    media: Brewery,
     Github: "https://github.com/HaleemH/The-Brewery-Discovery",
   },
   {
     name: "Urban Walls",
     stack: "React / Sinatra",
-    media: "",
+    media: UrbanWalls,
     Github: "https://github.com/HaleemH/Urban-Walls-FrontEnd",
   },
   {
     name: "Movie Rater",
     stack: "React ",
-    media: "",
+    media: MovieRater,
     Github: "https://github.com/HaleemH/phase2Project-Flatiron",
   },
   {
     name: "Fandom",
     stack: "Vanilla Javascript",
-    media: "",
+    media: FANDOM,
     Github: "https://github.com/HaleemH/Phase-1-project",
   },
 ];
